9cdccf8083fc0b220807c1da12a5c9f19c636d26
Egliss <egoparadise.rest@gmail.com>
2020-09-06 02:54:54 +0900

    Add benchmark project

---
+++ Paralleler_Bench/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Egliss.Paralleler;
+
+namespace Paralleler_Bench
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Program>();
+        }
+
+        [Benchmark(Description = "Egliss.Paralleler.OrderedParallel.ForAsync")]
+        public void ParallelerForAsync()
+        {
+            var token = new CancellationTokenSource();
+            var opt = new ParallelOptions()
+            {
+                CancellationToken = token.Token
+            };
+            OrderedParallel
+                .ForAsync(0, 100, opt, (int o) => { })
+                .Wait();
+        }
+
+        [Benchmark(Description = "System.Threading.Tasks.Parallel.For")]
+        public void ParallelFor()
+        {
+            var token = new CancellationTokenSource();
+            var opt = new ParallelOptions()
+            {
+                CancellationToken = token.Token
+            };
+            Parallel.For(0, 100, opt, (int o) => { });
+        }
+    }
+}
