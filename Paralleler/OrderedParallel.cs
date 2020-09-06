50d5e46ad91432e69043186dd7e28d91f29b2624
Egliss <egoparadise.rest@gmail.com>
2020-09-07 07:25:19 +0900

    Update optimize implement

--- Paralleler/OrderedParallel.cs
+++ Paralleler/OrderedParallel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,67 +7,35 @@ namespace Egliss.Paralleler
 {
     public class OrderedParallel
     {
-        private int _activeIndex = 0;
-        private readonly int _endIndex = 0;
-
-        public OrderedParallel(int endIndex)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async Task ForAsync(int beginIndex, int endIndex, Action<int> action)
         {
-            this._endIndex = endIndex;
-        }
-
-        public static async Task ForAsync(int beginIndex, int endIndex, ParallelOptions options, Action<int> action)
-        {
+            if (beginIndex >= endIndex)
+                return;
             if (action == null)
                 throw new ArgumentNullException(nameof(action));
-            if (options == null)
-                throw new ArgumentNullException(nameof(options));
-            if (endIndex <= beginIndex)
-                return;
 
-            var context = new OrderedParallel(endIndex);
-            var runnableCount = Math.Max(1, Math.Min(options.MaxDegreeOfParallelism, endIndex - beginIndex));
-            var tasks = new Task[runnableCount];
-            for (var index = 0; index < runnableCount; index++)
-            {
-                var next = Interlocked.Increment(ref context._activeIndex) - 1;
-                tasks[index] = Task.Run(() => context.RunNext(next, action, options.CancellationToken));
-            }
-            await Task.WhenAll(tasks);
+            await ParallelForContext.ForAsync(beginIndex, endIndex, action, -1);
         }
-        public static async Task ForAsync(int beginIndex, int endIndex, int threadCount, Action<int> action)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async Task ForAsync(int beginIndex, int endIndex, Action<int> action, int threadCount)
+        {
+            if (beginIndex >= endIndex)
+                return;
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
+
+            await ParallelForContext.ForAsync(beginIndex, endIndex, action, threadCount);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async Task ForAsync(int beginIndex, int endIndex, Action<int, CancellationToken> action, CancellationToken token, int threadCount = -1)
         {
             if (action == null)
                 throw new ArgumentNullException(nameof(action));
             if (endIndex <= beginIndex)
                 return;
 
-            var context = new OrderedParallel(endIndex);
-            var runnableCount = Math.Max(1, Math.Min(threadCount, endIndex - beginIndex));
-            var tasks = new Task[runnableCount];
-            for (var index = 0; index < runnableCount; index++)
-            {
-                var next = Interlocked.Increment(ref context._activeIndex) - 1;
-                tasks[index] = Task.Run(() => context.RunNext(next, action));
-            }
-            await Task.WhenAll(tasks);
-        }
-        private void RunNext(int index, Action<int> action, CancellationToken token)
-        {
-            action(index);
-            var next = Interlocked.Increment(ref this._activeIndex) - 1;
-            if (next >= this._endIndex)
-                return;
-
-            this.RunNext(next, action, token);
-        }
-        private void RunNext(int index, Action<int> action)
-        {
-            action(index);
-            var next = Interlocked.Increment(ref this._activeIndex) - 1;
-            if (next >= this._endIndex)
-                return;
-
-            this.RunNext(next, action);
+            await ParallelForContext.ForAsync(beginIndex, endIndex, action, token, threadCount);
         }
     }
 }
---
+++ Paralleler/ParallelForContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Egliss
+{
+    internal class ParallelForContext
+    {
+        private int _activeIndex = 0;
+        private readonly int _endIndex = 0;
+        private readonly int _runnerCount = 0;
+        public ParallelForContext(int begin, int end, int threadCount)
+        {
+            this._activeIndex = begin;
+            this._endIndex = end;
+            if (threadCount == -1)
+                threadCount = Environment.ProcessorCount / 2;
+            this._runnerCount = Math.Max(1, Math.Min(threadCount, end - begin));
+        }
+        public static async Task ForAsync(int beginIndex, int endIndex, Action<int> action, int threadCount)
+        {
+            var context = new ParallelForContext(beginIndex, endIndex, threadCount);
+            var tasks = new Task[context._runnerCount];
+            for (var index = 0; index < context._runnerCount; index++)
+            {
+                var next = Interlocked.Increment(ref context._activeIndex) - 1;
+                if (next >= context._endIndex)
+                    return;
+                tasks[index] = Task.Run(() => context.RunNext(next, action));
+            }
+            await Task.WhenAll(tasks);
+        }
+        public static async Task ForAsync(int beginIndex, int endIndex, Action<int, CancellationToken> action, CancellationToken token, int threadCount = -1)
+        {
+            var context = new ParallelForContext(beginIndex, endIndex, threadCount);
+            var tasks = new Task[context._runnerCount];
+            for (var index = 0; index < context._runnerCount; index++)
+            {
+                var next = Interlocked.Increment(ref context._activeIndex) - 1;
+                if (next >= context._endIndex)
+                    return;
+                tasks[index] = Task.Run(() => context.RunNext(next, action, token));
+            }
+            await Task.WhenAll(tasks);
+        }
+        private void RunNext(int index, Action<int> action)
+        {
+            action(index);
+            var next = Interlocked.Increment(ref this._activeIndex) - 1;
+            if (next >= this._endIndex)
+                return;
+
+            this.RunNext(next, action);
+        }
+        private void RunNext(int index, Action<int, CancellationToken> action, CancellationToken token)
+        {
+            action(index, token);
+            var next = Interlocked.Increment(ref this._activeIndex) - 1;
+            if (next >= this._endIndex)
+                return;
+
+            this.RunNext(next, action, token);
+        }
+    }
+}
