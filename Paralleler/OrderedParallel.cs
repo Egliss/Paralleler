c410ea0b67970a42eda33afec1772e04c5e82d23
Egliss <egoparadise.rest@gmail.com>
2020-09-09 04:54:46 +0900

    Add foreach implement

--- Paralleler/OrderedParallel.cs
+++ Paralleler/OrderedParallel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
@@ -31,6 +33,26 @@ namespace Egliss.Paralleler
 
             await ParallelForContext.ForAsync(beginIndex, endIndex, action, token, threadCount);
         }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async Task ForEachAsync<T>(IEnumerable<T> container, Action<T> action)
+        {
+            ThrowIfInvalidForEachArgument(container, action);
+            await ParallelForEachContext<T>.ForEachAsync(container, action, -1);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async Task ForEachAsync<T>(IEnumerable<T> container, Action<T> action, int threadCount)
+        {
+            ThrowIfInvalidForEachArgument(container, action);
+            await ParallelForEachContext<T>.ForEachAsync(container, action, threadCount);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async Task ForEachAsync<T>(IEnumerable<T> container, Action<T, CancellationToken> action, CancellationToken token, int threadCount = -1)
+        {
+            ThrowIfInvalidForEachArgument(container, action);
+            await ParallelForEachContext<T>.ForEachAsync(container, action, token, threadCount);
+        }
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static bool ThrowIfInvalidForArgument<T>(int beginIndex, int endIndex, T action) where T : class
         {
@@ -39,6 +61,14 @@ namespace Egliss.Paralleler
             if (action == null)
                 throw new ArgumentNullException(nameof(action));
             return true;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static void ThrowIfInvalidForEachArgument<T, U>(IEnumerable<T> container, U action) where U : class
+        {
+            if (container == null)
+                throw new ArgumentNullException(nameof(container));
+            if (action == null)
+                throw new ArgumentNullException(nameof(action));
         }
     }
 }
---
+++ Paralleler/ParallelForEachContext.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Egliss
+{
+    public class ParallelForEachContext<T>
+    {
+        private IEnumerator<T> _activeIterator = null;
+        private object _mutex = new object();
+        private readonly int _runnerCount = 0;
+        private readonly int _elementCount = 0;
+        
+        public ParallelForEachContext(IEnumerable<T> container, int threadCount)
+        {
+            this._activeIterator = container.GetEnumerator();
+            this._elementCount = container.Count();
+            if (threadCount == -1)
+                threadCount = Environment.ProcessorCount / 2;
+            this._runnerCount = Math.Max(1, Math.Min(threadCount, this._elementCount));
+        }
+        public static async Task ForEachAsync(IEnumerable<T> container, Action<T> action, int threadCount)
+        {
+            var context = new ParallelForEachContext<T>(container, threadCount);
+            var tasks = new Task[context._runnerCount];
+            for (var index = 0; index < context._runnerCount; index++)
+            {
+                tasks[index] = Task.Run(() => context.RunNext(action));
+            }
+            await Task.WhenAll(tasks);
+        }
+        public static async Task ForEachAsync(IEnumerable<T> container, Action<T, CancellationToken> action, CancellationToken token, int threadCount = -1)
+        {
+            var context = new ParallelForEachContext<T>(container, threadCount);
+            var tasks = new Task[context._runnerCount];
+            for (var index = 0; index < context._runnerCount; index++)
+            {
+                tasks[index] = Task.Run(() => context.RunNext(action, token));
+            }
+            await Task.WhenAll(tasks);
+        }
+        private void RunNext(Action<T> action)
+        {
+            IEnumerator<T> iterator;
+            lock (this._mutex)
+            {
+                this._activeIterator.MoveNext();
+                iterator = this._activeIterator;
+            }
+            var element = iterator.Current;
+            if (element == null)
+                return;
+
+            action(element);
+
+            this.RunNext(action);
+        }
+        private void RunNext(Action<T, CancellationToken> action, CancellationToken token)
+        {
+            IEnumerator<T> iterator;
+            lock (this._mutex)
+            {
+                this._activeIterator.MoveNext();
+                iterator = this._activeIterator;
+            }
+            var element = iterator.Current;
+            if (element == null)
+                return;
+            if (token.IsCancellationRequested)
+                return;
+
+            action(element, token);
+
+            this.RunNext(action, token);
+        }
+    }
+}
